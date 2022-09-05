04115d02957423b9808f5fdcecee6af0d4c46bab
artheeeee <88526727+artheeeee@users.noreply.github.com>
2022-09-05 16:01:10 +0800

    Delete routing.py

--- routing.py
+++
@@ -1,7 +0,0 @@
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-
-application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
-})
