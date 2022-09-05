1e9acd5191a013bfb3b247214879e621f282e5eb
artheeeee <88526727+artheeeee@users.noreply.github.com>
2022-09-05 17:24:29 +0800

    Update forms.py

--- nonprofit/forms.py
+++ nonprofit/forms.py
@@ -10,3 +10,20 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
+
+class BoxForm(ModelForm):
+
+    def clean(self):
+        cleaned_data = super(BoxForm, self).clean()
+        city = cleaned_data.get("city")
+        state = cleaned_data.get("state")
+
+        if not city == "Singapore":
+            raise forms.ValidationError("City must be 'Singapore'")
+        if not state == "Singapore":
+            raise forms.ValidationError("State must be Singapore")
+        return(cleaned_data)
+
+    class Meta:
+        model = RequestDonationBox
+        fields = ['name', 'email', 'phone', 'state', 'city', 'address', 'sub_group']
